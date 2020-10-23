d89b359110a50704566139b2f45eeecd3a6f2c78
Matt Jones <matt@boclips.com>
2020-10-23 12:30:38 +0100

    Setup tailwindcss
    We needed to introduce bundler, we chose webpack for now
    
    [#175077684]
    
    Signed-off-by: Andrew Bruce <me@andrewbruce.net>

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,27 @@
-module.exports = {};
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "src/index.html",
+    }),
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [new CssMinimizerPlugin()],
+  },
+};
