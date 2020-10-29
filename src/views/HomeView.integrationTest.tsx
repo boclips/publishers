d35796977c4579f4832ff4329ca9a2620df92c4a
Matt Jones <matt@boclips.com>
2020-10-29 09:37:29 +0000

    Appease linter

--- src/views/HomeView.integrationTest.tsx
+++ src/views/HomeView.integrationTest.tsx
@@ -1,12 +1,13 @@
 import { render } from '@testing-library/react';
-import React from 'react'
-import HomeView from "./HomeView";
+import React from 'react';
+import HomeView from './HomeView';
 
 describe('HomeView', () => {
- it('loads the home view text', () => {
-   const wrapper = render(<HomeView />)
+  it('loads the home view text', () => {
+    const wrapper = render(<HomeView />);
 
-   expect(wrapper.getByText("What video do you need today?")).toBeInTheDocument()
- })
-
-})
+    expect(
+      wrapper.getByText('What video do you need today?'),
+    ).toBeInTheDocument();
+  });
+});
