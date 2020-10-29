4d33b84c867adb594653a9bc4a19c4c334949463
Matt Jones <matt@boclips.com>
2020-10-29 15:08:45 +0000

    Allow us to analyse our build bundle
    [#175080010]

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -3,6 +3,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 const common = require('./webpack.common.js');
 
 const srcPath = path.resolve(__dirname, '../src');
@@ -15,12 +16,19 @@ module.exports = merge(common, {
     chunkFilename: '[name].[chunkhash:20].chunk.js',
   },
   plugins: [
-    new MiniCssExtractPlugin({ filename: '[name].[fullhash].css' }),
+    new MiniCssExtractPlugin({ filename: '[name].[fullhash:20].css' }),
     new HtmlWebpackPlugin({
       template: path.resolve(srcPath, 'index.html'),
     }),
   ],
   optimization: {
-    minimizer: [new CssMinimizerPlugin()],
+    runtimeChunk: {
+      name: 'manifest',
+    },
+    minimize: true,
+    minimizer: [
+      new CssMinimizerPlugin(),
+      new TerserPlugin({ terserOptions: { sourceMap: true }, parallel: true }),
+    ],
   },
 });
