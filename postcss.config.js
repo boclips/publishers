3ab9303e8d40392fa3cbe9b2285cd7f21381f394
Remigiusz Oleszczuk <remy@boclips.com>
2020-11-03 15:18:38 +0100

    Publishers setup
    - add antd
    - add postcss-import
    - add @boclips-ui/search-bar
    
    [#175394390]

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,13 @@
 const postcssPresetEnv = require('postcss-preset-env');
 const tailwind = require('tailwindcss');
 const autoprefixer = require('autoprefixer');
+const postcssImport = require('postcss-import');
 
 module.exports = {
-  plugins: [tailwind, autoprefixer, postcssPresetEnv({ stage: 0 })],
+  plugins: [
+    tailwind,
+    postcssImport,
+    autoprefixer,
+    postcssPresetEnv({ stage: 0 }),
+  ],
 };
--- src/components/SearchHero/index.tsx
+++ src/components/SearchHero/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import SearchBar from '@boclips-ui/search-bar';
 import HomeImageSVG from 'src/resources/home-image.svg';
 
 const SearchHero = () => {
   return (
     <main>
       <section className="grid bg-primary-light mt-10 grid-cols-12">
-        <div className="col-span-7 justify-center items-center" />
+        <div className="col-span-7 justify-center items-center">
+          <SearchBar onSearch={() => null} />
+        </div>
         <div className="col-span-5">
           <HomeImageSVG />
         </div>
