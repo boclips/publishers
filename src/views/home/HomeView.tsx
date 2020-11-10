d8ce5c01cf435cb426f7d23621930909ea5d46c0
Matt Jones <matt@boclips.com>
2020-11-10 19:42:19 +0000

    Rename back to the initial directory structure
    [#175394390]

--- src/views/home/HomeView.tsx
+++ src/views/home/HomeView.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import SearchHero from 'src/components/SearchHero';
+import SearchHero from 'src/components/searchHero/SearchHero';
 import { PageLayout } from 'src/components/layout/PageLayout';
 
 const HomeView = () => {
