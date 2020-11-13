3462131dc873718e8c9b58ad899a74f88847d6f7
Matt Jones <matt@boclips.com>
2020-11-13 17:55:54 +0000

    Fix HomeView not using the PageLayout component
    [#175394390]

--- src/views/home/HomeView.tsx
+++ src/views/home/HomeView.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
-import Navbar from 'src/components/layout/Navbar';
-import Footer from 'src/components/layout/Footer';
+import { PageLayout } from 'src/components/layout/PageLayout';
 import SearchHero from '../../components/searchHero/SearchHero';
 
 const HomeView = () => {
   return (
-    <div>
-      <Navbar />
-      <main className="container mx-auto">
-        <SearchHero />
-      </main>
-      <Footer />
-    </div>
+    <PageLayout>
+      <SearchHero />
+    </PageLayout>
   );
 };
 
