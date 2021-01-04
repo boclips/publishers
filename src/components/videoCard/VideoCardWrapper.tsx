e7d2c3c5a6ca288eb789af7e728fa4ac012489fa
Matt Jones <matt@boclips.com>
2021-01-04 11:43:17 +0000

    Update video-card component
    - We refactored how the badge uses css variables and theming so we need
    to reflect those changes. Publisher's app is now responsible for certain
    css properties of the badge

--- src/components/videoCard/VideoCardWrapper.tsx
+++ src/components/videoCard/VideoCardWrapper.tsx
@@ -5,6 +5,7 @@ import AddToCartButton from 'src/components/addToCartButton/AddToCartButton';
 import { VideoCardV2 } from '@boclips-ui/video-card-v2';
 import { PriceBadge } from 'src/components/videoCard/PriceBadge';
 import { VideoPlayer } from 'src/components/videoCard/VideoPlayer';
+import s from './VideoCardWrapper.module.less';
 
 interface Props {
   video: Video;
@@ -12,14 +13,16 @@ interface Props {
 
 export const VideoCardWrapper = ({ video }: Props) => {
   return (
-    <VideoCardV2
-      key={video.id}
-      video={convertVideoFromApi(video)}
-      videoPlayer={<VideoPlayer video={video} />}
-      border="bottom"
-      theme="publishers"
-      topBadge={<PriceBadge price={video.price?.displayValue} />}
-      actions={[<AddToCartButton videoId={video.id} key="cart-button" />]}
-    />
+    <div className={s.videoCard}>
+      <VideoCardV2
+        key={video.id}
+        video={convertVideoFromApi(video)}
+        videoPlayer={<VideoPlayer video={video} />}
+        border="bottom"
+        theme="publishers"
+        topBadge={<PriceBadge price={video.price?.displayValue} />}
+        actions={[<AddToCartButton videoId={video.id} key="cart-button" />]}
+      />
+    </div>
   );
 };
