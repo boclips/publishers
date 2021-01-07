b2d587f3a199957c2b63e9d773408279cb31e71c
Marcin Janik <marcin@boclips.com>
2021-01-07 17:37:25 +0100

    Change grid in emptyCart component
    -the image and text have less space between them
    
    [#175616093]
    
    Signed-off-by: Remigiusz Oleszczuk <remy@boclips.com>

--- src/components/cart/EmptyCart.tsx
+++ src/components/cart/EmptyCart.tsx
@@ -6,10 +6,10 @@ import s from './style.module.less';
 export const EmptyCart = () => {
   return (
     <div className={s.emptyCartView}>
-      <div className="col-start-2 col-end-9  flex justify-center items-center">
+      <div className="col-start-5 col-end-10 flex justify-center items-center">
         <EmptyCartCharacter />
       </div>
-      <div className="col-start-9 col-end-24 text-blue-800 flex flex-col justify-center items-center">
+      <div className="col-start-12 col-end-26 text-blue-800 flex flex-col justify-center">
         <h2>There are no items in your shopping cart</h2>
         <p className="text-h3 text-gray-700">
           Go to our <Link to="/">homepage</Link> and search for your perfect
