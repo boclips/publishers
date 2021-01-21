895d2bfa817fe87ea5130be9c7be4e11be9bc69d
Mima Farley-Rose <mima@boclips.com>
2021-01-21 15:26:56 +0000

    Add an 'Processing' status to any undelivered order
    [#176456567]

--- src/components/ordersTable/OrderDeliveryStatus.ts
+++ src/components/ordersTable/OrderDeliveryStatus.ts
@@ -2,5 +2,9 @@ import { OrderStatus } from 'boclips-api-client/dist/sub-clients/orders/model/Or
 
 export const orderDeliveryStatus = new Map([
   [OrderStatus.READY, 'PROCESSING'],
+  [OrderStatus.INVALID, 'PROCESSING'],
+  [OrderStatus.IN_PROGRESS, 'PROCESSING'],
+  [OrderStatus.INCOMPLETED, 'PROCESSING'],
+  [OrderStatus.CANCELLED, 'PROCESSING'],
   [OrderStatus.DELIVERED, 'DELIVERED'],
 ]);
