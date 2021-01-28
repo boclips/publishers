b6970a3572b86bb85d6714aa885a594fabad029c
Mima Farley-Rose <mima@boclips.com>
2021-01-28 17:19:18 +0000

    Fix pagination, ellipsis would disappear on hover
    [#176595770]

--- src/components/common/PaginationButtons.tsx
+++ src/components/common/PaginationButtons.tsx
@@ -1,22 +1,59 @@
 import PreviousArrow from 'src/resources/icons/pagination-prev-arrow.svg';
 import NextArrow from 'src/resources/icons/pagination-next-arrow.svg';
-import React from 'react';
+import Ellipsis from 'src/resources/icons/pagination-ellipsis.svg';
+import React, { ReactNode } from 'react';
 
+interface Props {
+  largeButton?: boolean;
+  children: ReactNode;
+  theme?: 'gray' | 'blue';
+}
+
+const CustomPaginationButton = ({
+  children,
+  largeButton,
+  theme = 'blue',
+}: Props) => {
+  return (
+    <div
+      className={`${largeButton && 'w-20'} 
+      ${theme === 'blue' && 'text-blue-800 border-gray-300'}
+      ${theme === 'gray' && 'text-gray-500 border-gray-400'}
+      flex flex-row font-bold border-2 h-9 rounded items-center px-2 justify-around`}
+    >
+      {children}
+    </div>
+  );
+};
 export const PaginationButtons = (_current, type, originalElement) => {
   if (type === 'prev') {
     return (
-      <div className="flex flex-row font-bold text-gray-500 border-2 rounded border-gray-400 items-center px-3 w-20 justify-between">
+      <CustomPaginationButton largeButton theme="gray">
         <PreviousArrow className="w-4" />
         <span>Prev</span>
-      </div>
+      </CustomPaginationButton>
+    );
+  }
+  if (type === 'jump-next' || type === 'jump-prev') {
+    return (
+      <CustomPaginationButton>
+        <Ellipsis className="w-3" />
+      </CustomPaginationButton>
+    );
+  }
+  if (type === 'page') {
+    return (
+      <CustomPaginationButton>
+        <span>{_current}</span>
+      </CustomPaginationButton>
     );
   }
   if (type === 'next') {
     return (
-      <div className="flex flex-row font-bold text-blue-800 border-2 rounded border-gray-300 items-center px-3 w-20 justify-between">
+      <CustomPaginationButton largeButton>
         <span>Next</span>
         <NextArrow className="w-4" />
-      </div>
+      </CustomPaginationButton>
     );
   }
   return originalElement;
