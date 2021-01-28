e8366b263863e7c29a0b7d17377ad5e37121d41d
Mima Farley-Rose <mima@boclips.com>
2021-01-28 11:13:32 +0000

    UI tweak to pagination
    [#176595770]

--- src/components/searchResults/VideoCardList.tsx
+++ src/components/searchResults/VideoCardList.tsx
@@ -3,6 +3,9 @@ import { List } from 'antd';
 import { PAGE_SIZE } from 'src/views/search/SearchResultsView';
 import React from 'react';
 import { VideoCardWrapper } from 'src/components/videoCard/VideoCardWrapper';
+import s from './VideoCardList.module.less';
+import NextArrow from '../../resources/icons/pagination-next-arrow.svg';
+import PreviousArrow from '../../resources/icons/pagination-prev-arrow.svg';
 
 interface Props {
   videos: Video[];
@@ -10,6 +13,26 @@ interface Props {
   handlePageChange: (page: number) => void;
   currentPage: number;
 }
+
+const itemRender = (_current, type, originalElement) => {
+  if (type === 'prev') {
+    return (
+      <div className="flex flex-row font-bold text-gray-500 border-2 rounded border-gray-400 items-center px-3 w-20 justify-between">
+        <PreviousArrow className="w-4" />
+        <span>Prev</span>
+      </div>
+    );
+  }
+  if (type === 'next') {
+    return (
+      <div className="flex flex-row font-bold text-blue-800 border-2 rounded border-gray-300 items-center px-3 w-20 justify-between">
+        <span>Next</span>
+        <NextArrow className="w-4" />
+      </div>
+    );
+  }
+  return originalElement;
+};
 
 export const VideoCardList = ({
   videos,
@@ -27,6 +50,8 @@ export const VideoCardList = ({
         showSizeChanger: false,
         onChange: handlePageChange,
         current: currentPage,
+        className: s.videoListPagination,
+        itemRender,
       }}
       dataSource={videos}
       renderItem={(video: Video) => (
