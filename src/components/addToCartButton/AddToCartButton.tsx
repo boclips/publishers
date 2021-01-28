d0fbc5a3f533e1502dfb2c88737b7bf57b599947
Alex Glen <alex@boclips.com>
2021-01-28 17:02:55 +0000

    Refactor common cart mutations out of components
    [#175616338]

--- src/components/addToCartButton/AddToCartButton.tsx
+++ src/components/addToCartButton/AddToCartButton.tsx
@@ -2,58 +2,24 @@ import { useMutation, useQueryClient } from 'react-query';
 import {
   doAddToCart,
   doDeleteFromCart,
+  useAddToCartQuery,
   useCartQuery,
+  useDeleteFromCartQuery,
 } from 'src/hooks/api/cartQuery';
-import { Cart } from 'boclips-api-client/dist/sub-clients/carts/model/Cart';
 import Button from '@boclips-ui/button';
 import React from 'react';
 import CartIcon from '../../resources/icons/cart-icon.svg';
-import { useBoclipsClient } from '../common/BoclipsClientProvider';
 
 interface AddToCartButtonProps {
   videoId: string;
 }
 
 const AddToCartButton = ({ videoId }: AddToCartButtonProps) => {
-  const queryClient = useQueryClient();
-  const boclipsClient = useBoclipsClient();
   const { data: cart } = useCartQuery();
+  const { mutate: mutateAddToCart } = useAddToCartQuery();
+  const { mutate: deleteFromCart } = useDeleteFromCartQuery();
 
   const cartItem = cart?.items?.find((it) => it?.videoId === videoId);
-
-  const { mutate: mutateAddToCart } = useMutation(
-    (id: string) => {
-      if (cartItem === undefined) {
-        return doAddToCart(cart as Cart, id, boclipsClient);
-      }
-      return Promise.reject(new Error('Item already in cart'));
-    },
-    {
-      onSuccess: (it) => {
-        queryClient.setQueryData('cart', (old: Cart) => ({
-          ...old,
-          items: [...old.items, it],
-        }));
-      },
-    },
-  );
-
-  const { mutate: mutateDeleteFromCart } = useMutation(
-    async (id: string) => {
-      if (cartItem) {
-        return doDeleteFromCart(cart as Cart, id, boclipsClient);
-      }
-      return Promise.reject(new Error('Item is not in cart'));
-    },
-    {
-      onSuccess: (it) => {
-        queryClient.setQueryData('cart', (old: Cart) => ({
-          ...old,
-          items: [...old.items.filter((item) => item.id !== it)],
-        }));
-      },
-    },
-  );
 
   const displayButton = () => {
     if (!cartItem) {
@@ -69,7 +35,7 @@ const AddToCartButton = ({ videoId }: AddToCartButtonProps) => {
     }
     return (
       <Button
-        onClick={() => mutateDeleteFromCart(cartItem.id)}
+        onClick={() => deleteFromCart(cartItem.id)}
         theme="publishers"
         type="secondary"
         text="Remove from cart"
