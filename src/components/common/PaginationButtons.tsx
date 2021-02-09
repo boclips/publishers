0809e0df9dacad5f7d6ddce03fb5441fa22e2780
Mima Farley-Rose <mima@boclips.com>
2021-02-09 13:54:23 +0000

    Refactoring pagination and adding hover and active states
    [#176595770]
    Signed-off-by: Remigiusz Oleszczuk <remy@boclips.com>

--- src/components/common/PaginationButtons.tsx
+++ src/components/common/PaginationButtons.tsx
@@ -1,30 +1,20 @@
-import PreviousArrow from 'src/resources/icons/pagination-prev-arrow.svg';
-import NextArrow from 'src/resources/icons/pagination-next-arrow.svg';
 import Ellipsis from 'src/resources/icons/pagination-ellipsis.svg';
 import React, { ReactNode } from 'react';
 import c from 'classnames';
+import s from './pagination.module.less';
+import Chevron from '../../resources/icons/grey-arrow.svg';
 
 interface Props {
   largeButton?: boolean;
   children: ReactNode;
-  theme?: 'gray' | 'blue';
 }
 
-const CustomPaginationButton = ({
-  children,
-  largeButton,
-  theme = 'blue',
-}: Props) => {
+const CustomPaginationButton = ({ children, largeButton }: Props) => {
   return (
     <div
-      className={c(
-        'flex flex-row font-bold border-2 h-9 rounded items-center px-2 justify-around',
-        {
-          'w-20': largeButton,
-          'text-blue-800 border-gray-300': theme === 'blue',
-          'text-gray-500 border-gray-400': theme === 'gray',
-        },
-      )}
+      className={c(s.paginationButton, {
+        [s.large]: largeButton,
+      })}
     >
       {children}
     </div>
@@ -34,31 +24,31 @@ const CustomPaginationButton = ({
 export const PaginationButtons = (current, type, _originalElement) => {
   if (type === 'prev') {
     return (
-      <CustomPaginationButton largeButton theme="gray">
-        <PreviousArrow className="w-4" />
-        <span>Prev</span>
+      <CustomPaginationButton largeButton>
+        <Chevron className={s.prev} />
+        <span className={s.copy}>Prev</span>
       </CustomPaginationButton>
     );
   }
   if (type === 'jump-next' || type === 'jump-prev') {
     return (
       <CustomPaginationButton>
-        <Ellipsis className="w-3" />
+        <Ellipsis className={s.ellipsis} />
       </CustomPaginationButton>
     );
   }
   if (type === 'page') {
     return (
       <CustomPaginationButton>
-        <span>{current}</span>
+        <span className={s.copy}>{current}</span>
       </CustomPaginationButton>
     );
   }
   if (type === 'next') {
     return (
       <CustomPaginationButton largeButton>
-        <span>Next</span>
-        <NextArrow className="w-4" />
+        <span className={s.copy}>Next</span>
+        <Chevron />
       </CustomPaginationButton>
     );
   }
