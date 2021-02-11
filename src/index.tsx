6aa3a10111600b2db0e9921eb326daa1b3c62d21
Andrew Bruce <me@andrewbruce.net>
2021-02-11 11:43:12 +0000

    Add sentry to app
    [#175398554]
    
    Signed-off-by: Alex Glen <alex@boclips.com>

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,28 @@ import ReactDom from 'react-dom';
 import BoclipsSecurity from 'boclips-js-security';
 import { ApiBoclipsClient } from 'boclips-api-client';
 import axios from 'axios';
+import { ExtraErrorData } from '@sentry/integrations';
+import * as Sentry from '@sentry/browser';
+import { Integrations } from '@sentry/tracing';
 import App from './App';
 import { Constants } from './AppConstants';
+
+if (process.env.NODE_ENV === 'production') {
+  const sentryRelease = process.env.SENTRY_RELEASE;
+
+  Sentry.init({
+    release: sentryRelease,
+    dsn:
+      'https://0e4298efe74b40fc88515bcde74b4b28@o236297.ingest.sentry.io/5632834',
+    integrations: [new Integrations.BrowserTracing(), new ExtraErrorData()],
+    tracesSampleRate: 1.0,
+    blacklistUrls: [
+      // Chrome extensions
+      /extensions\//i,
+      /^chrome:\/\//i,
+    ],
+  });
+}
 
 const authOptions = {
   realm: 'boclips',
