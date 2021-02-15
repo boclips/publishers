3715d56053429e4cbd836d450b0ee6d5c5e1a44e
Alex Glen <alex@boclips.com>
2021-02-15 16:08:11 +0000

    Fix styling on filter remove tag
    [#176552802]

--- src/components/filterPanel/SelectedFilterTag.tsx
+++ src/components/filterPanel/SelectedFilterTag.tsx
@@ -10,17 +10,17 @@ interface Props {
 
 export const SelectedFilterTag = ({ filter, removeFilter }: Props) => {
   return (
-    <span className="py-1 px-2 mr-2 mb-2 border-solid border-2 border-blue-700 rounded flex flex-nowrap items-center">
+    <span className="py-1 pl-2 mb-1 border-solid border-2 border-blue-700 rounded flex flex-nowrap items-center">
       {filter.name}
       <span
         role="button"
         data-qa="remove-filter"
-        className="ml-2"
+        className="mx-1 rounded p-1 text-gray-600 hover:text-blue-800 hover:border-blue-500 hover:bg-blue-400 h-5 w-5"
         tabIndex={0}
         onKeyPress={(_) => removeFilter(filter.key, filter.id)}
         onClick={() => removeFilter(filter.key, filter.id)}
       >
-        <CrossIconSVG className="text-blue-700 stroke-current stroke-2" />
+        <CrossIconSVG className="stroke-current  stroke-2 h-3 w-3" />
       </span>
     </span>
   );
