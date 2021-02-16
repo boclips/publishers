9a6bba5e69499d9cfd31ee99326235457639f991
Mima Farley-Rose <mima@boclips.com>
2021-02-16 15:03:17 +0000

    wip

--- src/components/common/copyLinkButton/CopyVideoLinkButton.tsx
+++ src/components/common/copyLinkButton/CopyVideoLinkButton.tsx
@@ -6,7 +6,10 @@ import CopyLinkIcon from 'src/resources/icons/copy-link-icon.svg';
 import { useBoclipsClient } from 'src/components/common/BoclipsClientProvider';
 import { buildVideoDetailsLink } from 'src/services/buildVideoDetailsLink';
 import s from './style.module.less';
+<<<<<<< HEAD
 import { trackCopyVideoShareLink } from '../analytics/Analytics';
+=======
+>>>>>>> wip
 
 interface Props {
   video: Video;
@@ -21,7 +24,13 @@ export const CopyVideoLinkButton = ({ video }: Props) => {
     <div className={`h-12 flex justify-end mt-2 ${s.copyLinkButton} mr-2`}>
       <CopyToClipboard text={link}>
         <Button
+<<<<<<< HEAD
           onClick={() => trackCopyVideoShareLink(video, apiClient)}
+=======
+          onClick={() =>
+            apiClient.events.trackVideoInteraction(video, 'COPY_SHARE_LINK')
+          }
+>>>>>>> wip
           text="Copy link"
           type="outline"
           width="100%"
