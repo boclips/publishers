41a0e02320d026aed25945587b9ee9d71ccf1479
Mima Farley-Rose <mima@boclips.com>
2021-02-18 17:37:35 +0000

    Fix cart-modal placement
    -CartModal was inside the cartOrderSummary
    
    [#176552608]

--- src/components/cart/CartOrderSummary.tsx
+++ src/components/cart/CartOrderSummary.tsx
@@ -21,36 +21,38 @@ export const CartOrderSummary = ({ videos }: Props) => {
   };
 
   return (
-    <div className="col-start-20 col-end-26">
-      <div className="border-blue-500 h-72 border-2 flex flex-col rounded p-5">
-        <div className="border-b border-blue-500 mb-4 font-normal text-base">
-          {renderCartSummaryItem(
-            <div>
-              Vide<span className="tracking-tightestest">o(s)</span> total
-            </div>,
-            getTotalPriceDisplayValue(videos),
-          )}
-          {renderCartSummaryItem('Captions', 'Free')}
-          {renderCartSummaryItem('Transcripts', 'Free')}
+    <>
+      <div className="col-start-20 col-end-26">
+        <div className="border-blue-500 h-72 border-2 flex flex-col rounded p-5">
+          <div className="border-b border-blue-500 mb-4 font-normal text-base">
+            {renderCartSummaryItem(
+              <div>
+                Vide<span className="tracking-tightestest">o(s)</span> total
+              </div>,
+              getTotalPriceDisplayValue(videos),
+            )}
+            {renderCartSummaryItem('Captions', 'Free')}
+            {renderCartSummaryItem('Transcripts', 'Free')}
+          </div>
+          <div className="flex font-bold text-lg text-gray-900 justify-between mb-6">
+            <span>Total</span>
+            <span data-qa="total-price">{`${getTotalPriceDisplayValue(
+              videos,
+            )}`}</span>
+          </div>
+          <Button
+            onClick={() => setModalOpen(!modalOpen)}
+            text="Place order"
+            height="44px"
+            width="100%"
+          />
         </div>
-        <div className="flex font-bold text-lg text-gray-900 justify-between mb-6">
-          <span>Total</span>
-          <span data-qa="total-price">{`${getTotalPriceDisplayValue(
-            videos,
-          )}`}</span>
-        </div>
-        <Button
-          onClick={() => setModalOpen(!modalOpen)}
-          text="Place order"
-          height="44px"
-          width="100%"
-        />
       </div>
       <OrderModal
         setOpen={setModalOpen}
         modalOpen={modalOpen}
         videos={videos}
       />
-    </div>
+    </>
   );
 };
