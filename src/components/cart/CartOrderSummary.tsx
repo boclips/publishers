2ae8b21e0c27ddba609b605f4bd156c5e34275ce
Marcin Janik <marcin@boclips.com>
2021-02-18 09:44:21 +0000

    Add captions and transcripts fields to cart summary
    [#176552606]
    
    Signed-off-by: Mima Farley-Rose <mima@boclips.com>

--- src/components/cart/CartOrderSummary.tsx
+++ src/components/cart/CartOrderSummary.tsx
@@ -11,15 +11,29 @@ interface Props {
 export const CartOrderSummary = ({ videos }: Props) => {
   const [modalOpen, setModalOpen] = useState<boolean>(false);
 
+  const renderAdditionalServicesSummary = (label) => {
+      return (
+          <div className="flex justify-between my-6">
+              <div>
+                  {label}
+              </div>
+              <div data-qa={"cart-summary-extra-services"}>Free</div>
+          </div>
+      )
+  }
   return (
     <>
-      <div className="col-start-20 col-end-26 border-blue-500 border-2 h-32 p-5 w-full justify-end flex flex-col rounded">
-        <div className="flex justify-between mb-6 font-normal text-base">
-          <div>
-            Vide<span className="tracking-tightestest">o(s)</span> total
+      <div className="col-start-20 col-end-26 border-blue-500 border-2 h-64 p-5 w-full justify-end flex flex-col rounded">
+          <div className={"border-b border-blue-500 mb-6 font-normal text-base"}>
+            <div className="flex justify-between">
+              <div>
+                Vide<span className="tracking-tightestest">o(s)</span> total
+              </div>
+              <div>{getTotalPriceDisplayValue(videos)}</div>
+            </div>
+              {renderAdditionalServicesSummary('Captions')}
+              {renderAdditionalServicesSummary('Transcripts')}
           </div>
-          <div>{getTotalPriceDisplayValue(videos)}</div>
-        </div>
         <Button
           onClick={() => setModalOpen(!modalOpen)}
           text="Place an order"
