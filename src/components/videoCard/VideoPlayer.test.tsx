7f08f507120681d7a0e482c95d29fb225ed3f1ea
Alex Glen <alex@boclips.com>
2021-02-25 14:52:27 +0000

    Fix build
    Signed-off-by: Mima Farley-Rose <mima@boclips.com>

--- src/components/videoCard/VideoPlayer.test.tsx
+++ src/components/videoCard/VideoPlayer.test.tsx
@@ -4,19 +4,11 @@ import { render } from '@testing-library/react';
 import { VideoFactory } from 'boclips-api-client/dist/test-support/VideosFactory';
 import { PlayerFactory } from 'boclips-player';
 import { mocked } from 'ts-jest/utils';
-import BoclipsSecurity from 'boclips-js-security';
 
 describe(`VideoPlayer`, () => {
   it('provides a token factory to the player that returns a valid token', async () => {
-    jest.spyOn(BoclipsSecurity, 'getInstance').mockImplementation(() => ({
-      logout: jest.fn(),
-      isAuthenticated: () => false,
-      getTokenFactory: () => () => Promise.resolve('test-token'),
-      configureAxios: jest.fn(),
-      ssoLogin: jest.fn(),
-    }));
-
     const video = VideoFactory.sample({ id: 'test-id' });
+
     render(<VideoPlayer video={video} />);
 
     expect(PlayerFactory.get).toHaveBeenCalled();
