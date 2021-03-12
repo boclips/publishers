7b1ad67737cf5a7d1093e82e8417b11ac987ef41
Mima Farley-Rose <mima@boclips.com>
2021-03-12 14:00:13 +0000

    Shows pageNotFoundError on video page when no video is found
    [#177271608]
    
    Signed-off-by: Eva Varga <eva@boclips.com>

--- src/views/video/VideoView.tsx
+++ src/views/video/VideoView.tsx
@@ -9,20 +9,27 @@ import { Helmet } from 'react-helmet';
 import { Layout } from 'src/components/layout/Layout';
 import { ErrorBoundary } from 'src/components/common/errors/ErrorBoundary';
 import RefreshPageError from 'src/components/common/errors/refreshPageError/RefreshPageError';
+import { PageNotFoundError } from 'src/components/common/errors/pageNotFoundError/pageNotFoundError';
+import { BoclipsApiError } from 'boclips-api-client/dist/types';
 
 const VideoView = () => {
   const videoId = useGetIdFromLocation('videos');
-  const { data: video, isLoading } = useFindOrGetVideo(videoId);
+  const { data: video, isLoading, error } = useFindOrGetVideo(videoId);
 
   // TODO: FIX INFINITE LOADING IF ERROR
 
   if (isLoading && !video) return <Loading />;
 
+  const errorMessage = error as BoclipsApiError;
+  const videoNotFound = errorMessage?.status === 404;
+
   return (
     <Layout dataQa="video-page" rowsSetup="grid-rows-video-view">
       {video?.title && <Helmet title={video.title} />}
       <Navbar showSearchBar />
-      <ErrorBoundary fallback={<RefreshPageError />}>
+      <ErrorBoundary
+        fallback={videoNotFound ? <PageNotFoundError /> : <RefreshPageError />}
+      >
         <VideoPage video={video} />
       </ErrorBoundary>
       <Footer />
