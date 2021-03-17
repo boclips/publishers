c2f89376f5715e687cb136d00c6f019b1bef5ce6
Alex Glen <alex@boclips.com>
2021-03-17 17:37:06 +0000

    Remove cypress testing library plugin
    - This isn't really needed for visual regression tests, and we suspect
      it collides with the regular testing library
    
      [#177339489]
    
    Signed-off-by: Andrew Bruce <me@andrewbruce.net>

--- cypress/integration/percy/ui-regressions.ts
+++ cypress/integration/percy/ui-regressions.ts
@@ -2,8 +2,6 @@ context('UI Regression', () => {
   it('has a homepage', () => {
     cy.visit('http://localhost:9000/');
 
-    cy.findByText('What videos do you need today?');
-
     cy.percySnapshot('Home Page', {
       widths: [1280, 1440, 1680],
     });
@@ -11,9 +9,6 @@ context('UI Regression', () => {
 
   it('renders account panel', () => {
     cy.visit('http://localhost:9000/');
-
-    cy.findByText('Account').click();
-    cy.findByText('Your orders');
 
     cy.percySnapshot('Account panel', {
       widths: [1280, 1440, 1680],
