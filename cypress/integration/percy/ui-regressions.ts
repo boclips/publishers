da55188aef34a8776906342f8452ba62929df18f
Andrew Bruce <me@andrewbruce.net>
2021-03-18 09:53:16 +0000

    Extract endpoint variable in percy tests
    [#177339489]
    
    Signed-off-by: Alex Glen <alex@boclips.com>

--- cypress/integration/percy/ui-regressions.ts
+++ cypress/integration/percy/ui-regressions.ts
@@ -1,6 +1,8 @@
 context('UI Regression', () => {
+  const endpoint = 'http://localhost:9000'
+
   it('has a homepage', () => {
-    cy.visit('http://localhost:9000/');
+    cy.visit(`${endpoint}/`);
 
     cy.percySnapshot('Home Page', {
       widths: [1280, 1440, 1680],
@@ -8,7 +10,7 @@ context('UI Regression', () => {
   });
 
   it('renders account panel', () => {
-    cy.visit('http://localhost:9000/');
+    cy.visit(`${endpoint}/`);
 
     cy.percySnapshot('Account panel', {
       widths: [1280, 1440, 1680],
