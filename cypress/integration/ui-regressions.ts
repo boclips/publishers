f02fe3d5f55dfcdf02cd279e493be236e25a1527
Andrew Bruce <me@andrewbruce.net>
2021-04-09 12:04:05 +0100

    WIP
    Signed-off-by: Matt Jones <matt@boclips.com>

--- cypress/integration/ui-regressions.ts
+++ cypress/integration/ui-regressions.ts
@@ -3,6 +3,7 @@ import {
   FacetsFactory,
 } from 'boclips-api-client/dist/test-support/FacetsFactory';
 import { SubjectFactory } from 'boclips-api-client/dist/test-support';
+import { Subject } from 'boclips-api-client/dist/types';
 
 context('UI Regression', () => {
   const endpoint = 'http://localhost:9000';
@@ -27,36 +28,48 @@ context('UI Regression', () => {
   it('should apply filters', () => {
     cy.visit(`${endpoint}/`);
 
-    cy.bo('create', 'video', {
-      title: 'orangutans doing maths',
-      subjects: [SubjectFactory.sample({ id: 'maths-id', name: 'maths' })],
+    const biology: Subject = SubjectFactory.sample({
+      id: 'biology-id',
+      name: 'Biology and Environmental Science',
     });
+    const geography: Subject = SubjectFactory.sample({
+      id: 'geography-id',
+      name: 'Geography and Earth Science',
+    })
+
+    cy .bo('create-fixture', 'video', biology, 'eel-sex');
+
     cy.bo('create', 'video', {
-      title: 'orangutans speaking english',
-      subjects: [SubjectFactory.sample({ id: 'english-id', name: 'english' })],
+      title: 'Eel with DOUBLE JAWS has one Nasty Bite!',
+      description:
+        `On this episode, Mark and the crew are back in Queensland, Australia for another epic Tide Pool adventure!` +
+        `As Mark explores, he comes across a Snowflake Eel - and this eel has double jaws.. and one nasty bite! ` +
+        `What other creatures do you think they will come across along the Australian coast? Watch now to find out!`,
+      subjects: [geography],
     });
+
     cy.bo(
       'set',
       'facets',
       FacetsFactory.sample({
         subjects: [
-          FacetFactory.sample({ id: 'maths-id', name: 'maths', hits: 10 }),
-          FacetFactory.sample({ id: 'english-id', name: 'english', hits: 10 }),
+          FacetFactory.sample({
+            id: biology.id,
+            name: biology.name,
+            hits: 1,
+          }),
+          FacetFactory.sample({
+            id: geography.id,
+            name: geography.name,
+            hits: 1,
+          }),
         ],
       }),
     );
-    cy.bo(
-      'create',
-      'subject',
-      SubjectFactory.sample({ name: 'english', id: 'english-id' }),
-    );
-    cy.bo(
-      'create',
-      'subject',
-      SubjectFactory.sample({ name: 'maths', id: 'maths-id' }),
-    );
+    cy.bo('create', 'subject', geography);
+    cy.bo('create', 'subject', biology);
 
-    cy.get('[data-qa="search-input"]').type('orangutans');
+    cy.get('[data-qa="search-input"]').type('eel');
     cy.get('button').contains('Search').click();
 
     cy.get('[data-qa="video-card-wrapper"]').should((videoCard) => {
@@ -67,7 +80,7 @@ context('UI Regression', () => {
       widths: [1280, 1440, 1680],
     });
 
-    cy.get('label').contains('maths').click();
+    cy.get('label').contains(biology.name).click();
     cy.get('[data-qa="video-card-wrapper"]').should((videoCard) => {
       expect(videoCard.length).to.equal(1);
     });
--- src/testSupport/bo.ts
+++ src/testSupport/bo.ts
@@ -14,6 +14,7 @@ import { Cart } from 'boclips-api-client/dist/sub-clients/carts/model/Cart';
 
 export interface Bo {
   create: {
+    fixture: (subject: Subject, name: VideoFixtureName) => void;
     video: (video: Partial<Video>) => void;
     subject: (subject: Subject) => void;
     cart: (cart: Partial<Cart>) => void;
@@ -23,6 +24,8 @@ export interface Bo {
     facets: (facet: Partial<VideoFacets>) => void;
   };
 }
+
+type VideoFixtureName = 'eel-sex' | 'eel-double-jaws';
 
 export const bo = (apiClient: FakeBoclipsClient): Bo => ({
   inspect: () => apiClient,
@@ -45,6 +48,69 @@ export const bo = (apiClient: FakeBoclipsClient): Bo => ({
   },
 
   create: {
+    fixture: (subject: Subject, name: VideoFixtureName) => {
+      switch (name) {
+        case 'eel-sex': {
+          const details: Partial<Video> = {
+            title:
+              'TED-Ed: No one can figure out how eels have sex | Lucy Cooke',
+            description:
+              `From Ancient Greece to the 20th century, Aristotle, Freud, and numerous other scholars were all looking for ` +
+              `the same thing: eel testicles. Freshwater eels could be found in rivers across Europe, but no one had ever seen ` +
+              `them mate and no researcher could find eel eggs or identify their reproductive organs. So how do eels reproduce, ` +
+              `and where do they do it? Lucy Cooke digs into the ancient mystery. [Directed by Anton Bogaty, narrated by Adrian ` +
+              `Dannatt, music by Jarrett Farkas].`,
+            subjects: [subject],
+          };
+          const fakeVideosClient = apiClient.videos as FakeVideosClient;
+          const videoURL =
+            'https://cdnapisec.kaltura.com/p/1776261/sp/177626100/playManifest/entryId/1_0jat7z15/format/applehttp/flavorParamIds/487041%2C487051%2C487061%2C487071%2C487081%2C487091%2C487111%2C1049881/protocol/https/video.mp4';
+
+          fakeVideosClient.insertVideo(
+            VideoFactory.sample({
+              id: 'blah',
+              title: 'test',
+              playback: PlaybackFactory.sample({
+                links: {
+                  createPlaybackEvent: null,
+                  createPlayerInteractedWithEvent: null,
+                  download: null,
+                  thumbnail: new Link({
+                    href:
+                      'https://cdnapisec.kaltura.com/p/1776261/thumbnail/entry_id/1_0jat7z15/width/1121/vid_slices/3/vid_slice/1',
+                  }),
+                  setThumbnailBySecond: null,
+                  setCustomThumbnail: null,
+                  deleteThumbnail: null,
+                  videoPreview: null,
+                  hlsStream: new Link({ href: videoURL }),
+                },
+              }),
+              links: {
+                self: new Link({
+                  href: videoURL,
+                  templated: false,
+                }),
+                logInteraction: new Link({
+                  href: videoURL,
+                  templated: false,
+                }),
+              },
+              ...details,
+            }),
+          );
+          break;
+        }
+
+        case 'eel-double-jaws': {
+          break;
+        }
+
+        default: {
+          break;
+        }
+      }
+    },
     subject: (subject: Subject) => {
       apiClient.subjects.insertSubject(subject);
     },
@@ -61,13 +127,28 @@ export const bo = (apiClient: FakeBoclipsClient): Bo => ({
     video: (video: Partial<Video>) => {
       const fakeVideosClient = apiClient.videos as FakeVideosClient;
       const videoURL =
-        'https://storage.googleapis.com/boclips-fixtures/IMG_7670.mp4';
+        'https://cdnapisec.kaltura.com/p/1776261/sp/177626100/playManifest/entryId/1_0jat7z15/format/applehttp/flavorParamIds/487041%2C487051%2C487061%2C487071%2C487081%2C487091%2C487111%2C1049881/protocol/https/video.mp4';
 
       fakeVideosClient.insertVideo(
         VideoFactory.sample({
           id: 'blah',
           title: 'test',
-          playback: PlaybackFactory.sample(),
+          playback: PlaybackFactory.sample({
+            links: {
+              createPlaybackEvent: null,
+              createPlayerInteractedWithEvent: null,
+              download: null,
+              thumbnail: new Link({
+                href:
+                  'https://cdnapisec.kaltura.com/p/1776261/thumbnail/entry_id/1_0jat7z15/width/1121/vid_slices/3/vid_slice/1',
+              }),
+              setThumbnailBySecond: null,
+              setCustomThumbnail: null,
+              deleteThumbnail: null,
+              videoPreview: null,
+              hlsStream: new Link({ href: videoURL }),
+            },
+          }),
           links: {
             self: new Link({
               href: videoURL,
