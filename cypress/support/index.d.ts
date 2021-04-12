1291bff5549e3c779ab42361b69ba63a93b68fe6
Matt Jones <matt@boclips.com>
2021-04-12 18:53:09 +0100

    Attempt to type cy.bo commands better
    - There is probably a more robust generic way to do this but I couldn't
    figure it out
    
    [#177565158]

--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -1,7 +1,21 @@
 /// <reference types="cypress" />
 
+// import { Bo } from '../../src/testSupport/bo';
+
+type PathTree<T> = {
+  [P in keyof T]: T[P] extends object ? [P?, ...Path<T[P]>] : [P?];
+};
+
+type Path<T> = PathTree<T>[keyof PathTree<T>];
+
 declare namespace Cypress {
+  type Bo = import('../../src/testSupport/bo').Bo;
+
   interface Chainable {
-    bo(verb: string, noun: string, value?: string): Chainable<Element>;
+    bo<T extends keyof Bo, U extends keyof Bo[T], V extends keyof Bo[T][U]>(
+      verb: T,
+      noun: U,
+      value?: V | any,
+    );
   }
 }
